cbc12dbb07f12b10c068159e2bcdc51ef6425c71
Jukka Paulin <jukka.paulin@gmail.com>
2021-02-26 19:14:08 +0200

    added console logging of requests

--- miniserv.js
+++ miniserv.js
@@ -5,7 +5,10 @@
 // the server responds by a signature string that tells about the 
 // underlying Node environment. 
 
-const myName = 'miniserv'
+// Started using versioned name on 25.2.2021
+const myName = 'miniserv v0.1.1'
+const listen_port = 8000
+
 var mySig    = undefined		// Main routine will set this
 var http     = require('http')
 
@@ -22,9 +25,20 @@ function mySignature() {
 	return `${myName}, running on Node ${process.version}\n`
 }
 
+/* 
+ * Validates a request route towards a static file: 
+ * whether the file path is legit, and file exists
+ * In 0.2.0 
+ */
+function validateFileRequest(fstr) {
+
+}
+
 // Creates the server's payload routine: this hooks to the port,
 // and gets run when a Web browser issues a "GET /" request. 
 var s = http.createServer(function(req,res) {
+	// This goes out to console, not to the client
+	console.log(`${req.method} ${req.url}`);
 	res.writeHead(200, { 'content-type': 'text/plain'});
 	res.end(`hello world! This comes from ${mySig}\n`);
 })
@@ -32,4 +46,5 @@ var s = http.createServer(function(req,res) {
 // Initialize the signature string once
 mySig = mySignature()
 // Put server in listening mode, waiting for incoming connections. 
-s.listen(8000);
+s.listen(listen_port);
+console.log(`Server listening on ${listen_port} for HTTP requests..`);
